af9be4803d02a69f1d32a0eb209f32b133ae7e2a
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-01-11 13:50:15 +0800

    Add UI components for forms

---
+++ frontend/src/components/ui/TextInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TextInput = ({ type, placeholder }) => {
+  return (
+    <input type={type} placeholder={placeholder}></input>
+  )
+}
+
+export default TextInput
