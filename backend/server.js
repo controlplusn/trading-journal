93d106f88c06be826ee8f5121f317d6e52171992
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-01-20 19:26:21 +0800

    Add routes for signup, login, and logout

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
-
-const PORT = 8000;
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     connectDB();
