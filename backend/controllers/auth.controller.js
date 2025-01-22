b2c5fe0876fcb41d9f6a94580d87be546b4f2d97
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-01-22 20:00:58 +0800

    Create the signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,37 @@
-export const signup = async (req, res) => {
-    res.send("Signup route");
-}
+import { User } from "../models/user.model";
+import bcrypt from 'bcryptjs';
 
+
+export const signup = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+        if(userAlreadyExists) {
+            return res.status(400).json({success: false, message: "User already exists"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            user,
+            password: hashedPassword,
+        })
+
+        await user.save();
+
+        // JWT token
+        generateToken(res, user._id);
+
+    } catch (error) {
+        return res.status(400).json({success: false, message: error.message});
+    }
+}
+ 
 export const login = async (req, res) => {
     res.send("Login route");
 }
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 8000;
 
-const app = express();
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
