2ba1e22515f81da19c60571f45d4780d5d5a6d71
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-01-23 15:21:20 +0800

    login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -43,9 +43,47 @@ export const signup = async (req, res) => {
 }
  
 export const login = async (req, res) => {
-    res.send("Login route");
+    const { email, password } = req.body; // taking input from the user
+
+    try {
+        const user = await User.findOne({ email }); // find the email of the user
+        console.log("User: ", user);
+        if (!user) {
+            res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+
+        // check if the password is valid
+        const isPasswordValid = await bcrypt.compare(password, user.password) // compare the user inputted password from the password on the database
+        if (!isPasswordValid) {
+            res.status(400).json({ success: false, message: "Invalid credentials" });
+        }
+
+        // generate JWT token
+        generateToken(res, user._id);
+
+        // update last login of user
+        user.lastLogin = new Date();
+
+        // save user profile on the database
+        await user.save();
+
+        // display the login profile if success
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined
+            }
+        });
+
+    } catch (error) {
+        console.log("Error in login ", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("Logout route");
+    res.clearCookie("token");
+    res.status(200).json({success: true, message: "Logged out sucessfully"});
 }
