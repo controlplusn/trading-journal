d5de52b853e2ad47aaf45b012bf2d78f8e165c72
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-01-28 15:48:28 +0800

    verifyToken middleware

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, verifyToken, checkAuth } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+router.get("/check-auth", verifyToken, checkAuth);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
