d36f8018bcae73768a970baebf054d6f034843cb
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-02-06 09:04:28 +0800

    Add datas on sidebar - Link to other components

--- frontend/src/pages/Sidebar/Sidebar.jsx
+++ frontend/src/pages/Sidebar/Sidebar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <h2>Trading Journal</h2>
+      </div>
+
+      <ul className='sidebar-list'>
+        {SidebarData.map((item, index) => (
+          <li key={index} className='sidebar-item'>
+            <Link to={item.link} className='sidebar-link'>
+              {item.title}
+            </Link>
+          </li>
+        ))}
+
+      </ul>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Sidebar/SidebarData.js
@@ -0,0 +1,19 @@
+
+export const SidebarData = [
+    {
+        title: "Dashboard",
+        link: "/dashboard"
+    },
+    {
+        title: "Calendar",
+        link: "/calendar"
+    },
+    {
+        title: "Trades",
+        link: "/tradecollection"
+    },
+    {
+        title: "Analytics",
+        link: "/analytics"
+    }
+]
