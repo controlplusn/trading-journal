162cac8e4b4f8d62bbe6d09c9b6ac132bc1cec8f
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-02-27 12:30:48 +0800

    Add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/trade", tradeRoutes);
