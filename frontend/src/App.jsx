b8a075def6a2bbc54425fa72b292da86f1c7d55d
Carle Medina <22-06230@g.batstate-u.edu.ph>
2025-03-06 07:56:55 +0800

    Add error element on route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,27 @@ import Sidebar from './pages/Sidebar/Sidebar';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import TradeEntryButton from './pages/Trade Entry Form/TradeEntryButton';
 import TradeFormParent from './pages/Trade Entry Form/TradeFormParent';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Trade from './pages/Trade/Trade';
+import NotFoundPage from './pages/Error Page/NotFoundPage';
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <TradeFormParent />
+      element: <Trade />,
+      errorElement: <NotFoundPage />
     },
     {
       path: '/signup',
-      element: <Signup />
-    }
+      element: <Signup />,
+      errorElement: <NotFoundPage />
+    },
+    {/* {
+      path: 'tradesPage',
+      element: <Trade />
+    }, */}
   ])
 
   return (
